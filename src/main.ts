b3c2ac4f641316e285157901bc82fe5b0eb685d3
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-10 21:25:00 +0200

    BoilerPlate code for nestjs

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  //app.setGlobalPrefix('api');
+  app.enableCors();
+  app.use(cookieParser())
+  await app.listen(3000);
+}
+bootstrap();
