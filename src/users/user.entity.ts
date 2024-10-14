300f1e97a4b06797cb1335bdcf42848e4ecc5895
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:10:53 +0200

    mapped user entity to a post entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,18 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import Post from 'src/posts/posts.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 class User {
-    @PrimaryGeneratedColumn()
-    public id?: number;
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @Column({unique: true})
-    public email: string;
+  @Column({ unique: true })
+  public email: string;
 
-    @Column()
-    public name: string;
+  @Column()
+  public name: string;
 
-    @Column()
-    public password: string;
+  @Column()
+  public password: string;
+
+  @OneToMany(() => Post, (post: Post) => post.author)
+  public posts: Post[];
 }
 
 export default User;
