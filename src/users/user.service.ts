f2eec85654d9d7ddda9daa44ac6da5d9acaae65e
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:11:49 +0200

    implemented a getallusers function

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,11 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import User from './user.entity';
 import { Repository } from 'typeorm';
-import CreateUserDto from './dto/createUser.dto';
+import { CreateUserDto } from './dto';
+import User from './user.entity';
 
 @Injectable()
-export class UserService {
+export default class UserService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -13,13 +13,8 @@ export class UserService {
 
   async getByEmail(email: string) {
     const user = await this.usersRepository.findOneBy({ email: email });
-    if (user) {
-      return user;
-    }
-    throw new HttpException(
-      'User with this email does not exist',
-      HttpStatus.NOT_FOUND,
-    );
+    return user;
+    
   }
 
   async create(userData: CreateUserDto) {
@@ -34,5 +29,10 @@ export class UserService {
       return user
     }
     throw new HttpException("User with this id does not exist", HttpStatus.NOT_FOUND)
+  }
+
+  async getAllusers(){
+    const users = await this.usersRepository.find();
+    return users;
   }
 }
