1b849b08eb7beacfa307b82a747b2ccf7190a556
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:09:52 +0200

    exported post service

--- src/Posts/posts.module.ts
+++ src/Posts/posts.module.ts
@@ -7,6 +7,7 @@ import Post from './posts.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService],
+  exports: [PostsService]  // Make PostsService available to other modules for dependency injection
 })
 export class PostsModule {}
