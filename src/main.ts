40f0c0d33e3dae71126621cf3e07e2d4459180d1
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:00:01 +0200

    set the whitelist property of validation pipe to true

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({skipMissingProperties: true}));
+  app.useGlobalPipes(
+    new ValidationPipe({ skipMissingProperties: true, whitelist: true }),
+  );
   //app.setGlobalPrefix('api');
   app.enableCors();
   app.use(cookieParser());
