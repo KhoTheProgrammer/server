6fc076584409c530ff85e32d282cfb7cf639a009
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:01:08 +0200

    implemented the signin and signup functions without jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,51 +1,26 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import PostsService from 'src/posts/posts.service';
+import { CreateUserDto } from 'src/users/dto';
+import UserService from 'src/users/user.service';
 import { AuthService } from './auth.service';
-import RegisterDto from './dto/register.dto';
-import { LocalAuthGuard } from './localAuth.guard';
-import RequestWithUser from './requestWithUser.interface';
-import { Response } from 'express';
-import JwtAuthGuard from './jwt-auth.guard';
+import { SigninDto } from './dto/signin.dto';
+
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authservice: AuthService) {}
-
-  @Post('register')
-  async register(@Body() registrationData: RegisterDto) {
-    return this.authservice.register(registrationData);
+  constructor(
+    private postservice: PostsService,
+    private usersService: UserService,
+    private authservice: AuthService
+  ) {}
+  @Post('signup')
+  signup(@Body() signupdata: CreateUserDto) {
+    console.log(signupdata);
+    return this.authservice.signup(signupdata);
   }
 
-  @HttpCode(200)
-  @UseGuards(LocalAuthGuard)
-  @Post('log-in')
-  async logIn(@Req() request: RequestWithUser, @Res() response: Response) {
-    const { user } = request;
-    const cookie = this.authservice.getCookieWithJwtToken(user.id);
-    user.password = undefined;
-    return response.setHeader('Set-Cookie', cookie);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('log-out')
-  async logout(@Req() request: RequestWithUser, @Res() response: Response) {
-    response.setHeader('Set-Cookie', this.authservice.getCookieForLogOut());
-    return response.sendStatus(200);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get()
-  authenticate(@Req() request: RequestWithUser) {
-    const user = request.user;
-    user.password = undefined;
-    return user;
+  @Post('signin')
+  signin(@Body() signinata: SigninDto){
+    return this.authservice.signin(signinata);
   }
 }
