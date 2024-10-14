223e397618aa95c59880ed4143a85e9f0dd4d1f0
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:01:48 +0200

    removed  jwt configurations

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,30 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { UserModule } from 'src/users/user.module';
-import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from './jwt.strategy';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/users/user.module';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
-  imports: [
-    UserModule,
-    PassportModule,
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JW_SECRET'),
-        signOptions: {
-          expiresIn: `${configService.get('JW_EXPIRATION_TIME')}s`,
-        },
-      }),
-    }),
-  ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [UserModule, PostsModule],
   controllers: [AuthController],
+  providers: [AuthService]
 })
 export class AuthModule {}
