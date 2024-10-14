ed374faec13bfc8b9a09285c13a9f6591331abe5
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-14 18:12:17 +0200

    Validated the data for signup

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,7 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
-
-export default CreateUserDto;
