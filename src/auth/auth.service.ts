434f5468e4f465e4941b8476fe9d1bf4fedbeed2
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-10-17 07:30:55 +0200

    implemented a function to generate a token and return it instead of a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import UserService from 'src/users/user.service';
 import { SigninDto } from './dto/signin.dto';
 import * as bcrypt from 'bcrypt';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(signupdata: CreateUserDto) {
     //generate the password hash
     const hash = await bcrypt.hash(signupdata.password, 10);
@@ -26,7 +32,7 @@ export class AuthService {
     if (!user) {
       throw new HttpException('Incorrect credentials', HttpStatus.NOT_FOUND);
     }
-    
+
     // compare password
     const ismatch = await bcrypt.compare(signindata.password, user.password);
 
@@ -34,7 +40,27 @@ export class AuthService {
     if (!ismatch) {
       throw new HttpException('Incorrect credentials', HttpStatus.NOT_FOUND);
     }
-    //return user
-    return user;
+    //return token
+    return this.signToken(user.id, user.email);
+  }
+
+  //Generate json web token
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwtService.signAsync(payload, {
+      secret: this.config.get('JWT_SECRET'),
+      expiresIn: this.config.get('JWT_EXPIRATION_TIME'),
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
