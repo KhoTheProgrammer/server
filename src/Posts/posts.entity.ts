87dd7e9b333e6ed13ff9a670c3e4087b6790fd0b
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-12-30 20:28:49 +0200

    commented out category column for debbuging purposes

--- src/Posts/posts.entity.ts
+++ src/Posts/posts.entity.ts
@@ -1,7 +1,5 @@
-import { Transform } from 'class-transformer';
-import Category from '../Category/category.entity';
 import User from '../users/user.entity';
-import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 class Post {
@@ -14,20 +12,20 @@ class Post {
   @Column()
   public content: string;
 
-  @Column({ nullable: true })
-  @Transform(({ value }) => {
-    if (value !== null) {
-      return value;
-    }
-  })
-  public category?: string;
+  // @Column({ nullable: true })
+  // @Transform(({ value }) => {
+  //   if (value !== null) {
+  //     return value;
+  //   }
+  // })
+  // public category?: string;
 
   @ManyToOne(() => User, (author: User) => author.posts)
   public author: User
 
-  @ManyToMany(() => Category)
-  @JoinTable()
-  public categories: Category[]
+  // @ManyToMany(() => Category)
+  // @JoinTable()
+  // public categories: Category[]
 }
 
 export default Post;
