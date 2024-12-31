530130197264dd755f3b8ae74ab1c9647b19fdaa
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-12-31 23:47:46 +0200

    Implemented a funtion to reset cookies after logout

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -119,4 +119,11 @@ export class AuthService {
       );
     }
   }
+
+  public getCookiesForLogOut() {
+    return [
+      'Authentication=; HttpOnly; Path=/; Max-Age=0',
+      'Refresh=; HttpOnly; Path=/; Max-Age=0',
+    ];
+  }
 }
