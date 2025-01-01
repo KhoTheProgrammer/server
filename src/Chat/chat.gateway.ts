be177a356bfe1b690d545959cf9a366658a8b063
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-01 23:03:17 +0200

    Implemented some functionalities

--- src/Chat/chat.gateway.ts
+++ src/Chat/chat.gateway.ts
@@ -1,17 +1,26 @@
 import {
+    ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { ChatService } from './chat.service';
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
+  constructor(private readonly chatService: ChatService) {}
+
+  async handleConnection(socket: Socket) {
+    await this.chatService.getUserFromSocket(socket);
+  }
   @SubscribeMessage('send_message')
-  listenForMessages(@MessageBody() data: string) {
-    this.server.sockets.emit('receive_message', data);
+  async listenForMessages(@MessageBody() content: string, @ConnectedSocket() socket: Socket) {
+    const author = await this.chatService.getUserFromSocket(socket);
+    this.server.sockets.emit('receive_message', {content, author});
   }
 }
